623dcc8c721452c71740101ffcfd79c2364dc22f
durielbaal <michaelogwu1997@gmail.com>
2024-10-14 20:15:38 +0200

    2. Adding more info to general data of pokemon api: more attributes to pokemonDto

--- microservice-pokeapi-lib/src/main/java/com/myke/studios/dto/PokemonDto.java
+++ microservice-pokeapi-lib/src/main/java/com/myke/studios/dto/PokemonDto.java
@@ -1,8 +1,8 @@
 package com.myke.studios.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,6 +16,24 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PokemonDto implements Serializable {
+
+  /**
+   * Pokedex number of pokemon.
+   */
+  @JsonProperty("id")
+  public int pokedexId;
+  /**
+   * Name of pokemon.
+   */
+  public String name;
+  /**
+   * Height of pokemon.
+   */
+  public double height;
+  /**
+   * Weight of pokemon.
+   */
+  public double weight;
   /**
    * List of abilities.
    */
