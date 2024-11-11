77a5ba1fc76db861d08b25587ac995e0ca411d8d
durielbaal <michaelogwu1997@gmail.com>
2024-11-11 13:28:52 +0100

    7:optimized imports

--- microservice-security-api/src/test/java/com/myke/studios/test/ut/JwtSecurityTest.java
+++ microservice-security-api/src/test/java/com/myke/studios/test/ut/JwtSecurityTest.java
@@ -2,7 +2,6 @@ package com.myke.studios.test.ut;
 
 import com.myke.studios.Initializer;
 import com.myke.studios.jwt.JwtService;
-import io.jsonwebtoken.lang.Classes;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
